091f77822643f74928dbe8ac3c1e35d32ee35308
Peter <hello.ship.code@gmail.com>
2025-01-27 15:44:33 +0530

    its over

--- calculator-app/index.js
+++ calculator-app/index.js
@@ -1,11 +1,20 @@
 "use strict";
 const btn = document.querySelectorAll('button');
+const input = document.getElementById('input');
 const result = document.getElementById('result');
+let output = "";
 btn.forEach((button) => {
     button.addEventListener('click', () => {
-        if (result) {
-            console.log(`Button clicked: ${button.textContent}`);
-            result.textContent = button.textContent;
+        if (input && button.textContent !== "=") {
+            output += (button.textContent);
+            input.textContent = output;
+        }
+        if (button.textContent === "=" && result && input) {
+            result.textContent = (eval(output));
+        }
+        if (button.textContent === "clear" && input && result) {
+            input.textContent = "";
+            result.textContent = "";
         }
     });
 });
--- calculator-app/index.ts
+++ calculator-app/index.ts
@@ -1,11 +1,28 @@
 const btn = document.querySelectorAll('button') as NodeListOf<HTMLButtonElement>;
+const input = document.getElementById('input') as HTMLButtonElement | null;
 const result = document.getElementById('result') as HTMLButtonElement | null;
 
+
+let output = "";
 btn.forEach((button) => {
-    button.addEventListener('click', () => {
-        if(result){
-            console.log(`Button clicked: ${button.textContent}`);
-            result.textContent = result?.textContent + button?.textContent;
+    button.addEventListener('click',
+        () => {
+        if(input && button.textContent !== "="){
+            output += (button.textContent);
+            input.textContent = output;
+            
+        }
+        if( button.textContent === "=" && result && input){
+            result.textContent = (eval(output)); 
+        }
+        if(button.textContent === "clear" && input && result)
+        {
+            input.textContent = "";
+            result.textContent = "";
         }
     });
-});
+}
+);
+
+
+
