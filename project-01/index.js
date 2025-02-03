e38def62057613f05ac384b12b1e3d30f4df6db1
Peter <hello.ship.code@gmail.com>
2025-02-03 16:33:01 +0530

    1.1.3

--- project-01/index.js
+++ project-01/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 2010;
+const PORT = 3000;
 const users = require('./MOCK_DATA.json');
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
