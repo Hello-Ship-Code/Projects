6890f35ca2260ac09bed9b454ecaf38187456965
Hello Ship <hello.ship.code@gmail.com>
2025-02-04 00:42:46 +0530

    .env

--- project-01/index.js
+++ project-01/index.js
@@ -3,18 +3,21 @@ import { userRoutes } from "./routes/user.js"
 import { connect } from "./connection.js";
 import { logreq } from "./middlewares/index.js";
 
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/project-01";
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 
-logreq("log.txt");
+logreq("./log.txt");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connection
-connect("mongodb://localhost:27017/project-01").then(() => console.log("Connected to database..."));
+connect(mongoUrl).then(() => console.log("Connected to database..."));
 
 // Use the userRoutes
 app.use("/users", userRoutes);;
